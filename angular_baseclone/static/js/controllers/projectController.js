f68b35d71768a8eea271656e0e11e07e6244e8c6
Baylee Feore <baylee@yetihq.com>
2014-04-08 14:33:41 -0700

    Using a custom  function for angular's filter

--- angular_baseclone/static/js/controllers/projectController.js
+++ angular_baseclone/static/js/controllers/projectController.js
@@ -20,6 +20,26 @@ function projectController($scope, $http, $routeParams, $resource) {
           console.log("didn't work");
     });
 
+    $scope.hasAttachment = function(filterData){
+        // When writing your own function to be used by Angular's filter,
+        // the argument it takes (in this case filterData, though you can call it whatever you want)
+        // represents each object that is going through the filter.
+        // So, in this example, you're doing an ngRepeat on topic in topics (in the view)
+        // filterData is each topic, not the list of all topics being filtered
+        // If the function returns true for that object, then it 'passes' the filter
+        // If the function returns false for that object, then it does not pass the filter and will not
+        // be iterated over.
+
+        // If the checkbox is checked (when checked, it's value is true)
+        if ($scope.attachmentBoolean) {
+            // Only return topics that have attachments
+            return filterData.attachments > 0;
+        // else return true for all topics
+        } else {
+            return true
+        }
+    };
+
     var messageApi = $resource('/proxy/projects/'+ projectId +'/messages/:id.json', {
         // parameter defaults
         id: '@id'
