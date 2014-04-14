3655b844caa518d3a82336b55f1704b83910b306
Baylee Feore <baylee@yetihq.com>
2014-04-14 13:45:52 -0700

    Simple  example

--- angular_example/static/js/controllers/exampleController.js
+++ angular_example/static/js/controllers/exampleController.js
@@ -1,3 +1,16 @@
-function exampleController($scope) {
+function exampleController($scope, $http) {
     $scope.stuff = "I have a default value";
+
+    $http({method: 'GET', url: 'http://api.icndb.com/jokes/random?firstName=Rudy&lastName=Mutter'}).
+    success(function(data, status, headers, config) {
+      // this callback will be called asynchronously
+      // when the response is available
+            $scope.chuckNorris = data.value.joke;
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+            $scope.chuckNorris = "Rudy Mutter never fails";
+    });
+
 }
